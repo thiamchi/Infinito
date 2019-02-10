af3e8c32183e0870f377e0444fa8b47a676e5134
thiamchi <35895671+thiamchi@users.noreply.github.com>
2019-02-10 22:26:48 +0800

    Add WPF

---
+++ Infinito/Infinito.WPF/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms.Platform.WPF;
+using Xamarin.Forms;
+
+namespace Infinito.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new Infinito.App());
+        }
+    }
+}
